dc6983147b610efea2229b1f4a7438dc261b3e60
Sean Zheng <SeanZheng21@gmail.com>
2021-08-03 16:23:18 +0800

    Initial Commit

---
+++ Game of SKATE/Game_of_SKATEApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_of_SKATEApp.swift
+//  Game of SKATE
+//
+//  Created by Carlistle ZHENG on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_of_SKATEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game of SKATETests/Game_of_SKATETests.swift
@@ -0,0 +1,33 @@
+//
+//  Game_of_SKATETests.swift
+//  Game of SKATETests
+//
+//  Created by Carlistle ZHENG on 8/3/21.
+//
+
+import XCTest
+@testable import Game_of_SKATE
+
+class Game_of_SKATETests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
